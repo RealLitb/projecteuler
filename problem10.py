f751cda2b48e4656f373ac608bf3f41fa1de685f
ud52abyp <schaub.johannes@googlemail.com>
2017-03-13 11:15:49 +0100

    three new ones

--- problem10.py
+++ problem10.py
@@ -1,11 +1,10 @@
 import itertools
 
 primes=[2]
-for i in range(3, 2000000):
+for i in range(3, 2000000, 2):
     for p in itertools.takewhile((lambda p: p*p <= i), primes):
         if i % p == 0:
             break
     else:
         primes.append(i)
-    i += 2
 print("sum of primes < 2000000", sum(primes))
